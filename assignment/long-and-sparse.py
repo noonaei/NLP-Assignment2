a4028ae553bbcae1f38c35b7518c731e4af382f4
MichaelGurevich <michaelgur273@gmailc.com>
2025-12-16 15:41:22 +0200

    initial implementation of creating the long and sparse co-occurrence matrix

--- assignment/long-and-sparse.py
+++ assignment/long-and-sparse.py
@@ -3,6 +3,9 @@
 import time
 import string
 from collections import defaultdict
+from operator import indexOf
+
+import numpy as np
 
 
 MAX_LINES = 8000000  # 8 million lines
@@ -90,3 +93,42 @@ def get_vocab(word2freq, vocab_size):
         # Extract only the words w/o the frequencies
         return {word for word, freq in top_words}
 
+
+def create_co_occurrence_mat(corpus_file_name, data:list, vocab:list, window_size):
+    nrows = len(data)
+    ncols = len(vocab)
+
+    data_lookup = set(data)  # Set for O(1) membership checking
+    data_lookup_dict = {word: idx for idx, word in enumerate(data)}  # O(1) word-to-row-index lookup
+    vocab_lookup = {word: idx for idx, word in enumerate(vocab)}  # O(1) word-to-column-index lookup
+    co_occurrence_mat = np.zeros((nrows, ncols))
+
+    # go over the corpus
+    # when hit a word that is in the data - check which words accompanying it
+    with open(corpus_file_name, 'r', encoding='utf-8') as fin:
+        # Iterate over the file line by line
+        for line in fin:
+            cleaned_line = clean_line(line).split() # todo: when reading corpus the first time should create a cleaned corpus pickle file for faster I/O
+            cleaned_line_len = len(cleaned_line)
+
+            for index, word in enumerate(cleaned_line):
+                if word in data_lookup: # if we hit a word which is in the data then increment the words accompanying it
+
+                    # increment the words in the left window
+                    word_index = data_lookup_dict[word]
+                    for i in range(index-window_size, index):
+                        if i >= 0:
+                            context_word = cleaned_line[i]
+                            if context_word in vocab_lookup:
+                                co_occurrence_mat[word_index, vocab_lookup[context_word]] += 1
+
+                    # increment the words in the right window
+                    for i in range(index+1, index+window_size+1):
+                        if i >= cleaned_line_len:
+                            break
+                        else:
+                            context_word = cleaned_line[i]
+                            if context_word in vocab_lookup:
+                                co_occurrence_mat[word_index, vocab_lookup[context_word]] += 1
+
+    return co_occurrence_mat
